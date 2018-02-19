4f3009c3c9788b2fb2cf044913d7d0b35f1a3eaf
Satoshi Hikida <s.hikida@hotmail.co.jp>
2018-02-19 09:36:25 +0900

    add main class

---
+++ src/main/java/my/example/spring/security/Application.java
@@ -0,0 +1,14 @@
+package my.example.spring.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by satoshi on 2018/02/19.
+ */
+@SpringBootApplication
+public class Application {
+  public static void main(String... args) {
+    SpringApplication.run(Application.class, args);
+  }
+}
