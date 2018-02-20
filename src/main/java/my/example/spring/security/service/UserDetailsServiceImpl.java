10435913c9113477dd4abd7cee7167db9ceafe40
Satoshi Hikida <s.hikida@hotmail.co.jp>
2018-02-20 20:07:34 +0900

    add password encoder

--- src/main/java/my/example/spring/security/service/UserDetailsServiceImpl.java
+++ src/main/java/my/example/spring/security/service/UserDetailsServiceImpl.java
@@ -1,23 +1,16 @@
 package my.example.spring.security.service;
 
-import my.example.spring.security.model.UserDetailsImpl;
-import my.example.spring.security.model.UserDetailsRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-
 /**
  * Created by satoshi on 2018/02/20.
  */
-public class UserDetailsServiceImpl implements UserDetailsService {
-
-  @Autowired
-  private UserDetailsRepository userDetailsRepository;
-
-  @Override
-  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-    UserDetailsImpl user = userDetailsRepository.findByUsername(username);
-    return user;
-  }
-}
+//public class UserDetailsServiceImpl implements UserDetailsService {
+//
+//  @Autowired
+//  private UserDetailsRepository userDetailsRepository;
+//
+//  @Override
+//  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//    UserDetailsImpl user = userDetailsRepository.findByUsername(username);
+//    return user;
+//  }
+//}
